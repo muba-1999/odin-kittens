f1624713ea6cbd9f91746a6dbda8c6ad42fcb9d4
mubanga <mukobam4@gmail.com>
2022-09-07 20:35:51 +0200

    restful actions

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -4,7 +4,7 @@ class KittensController < ApplicationController
     end
 
     def show
-        @kitten = kitten.find(params[:id])
+    
     end
 
     def new
@@ -15,28 +15,34 @@ class KittensController < ApplicationController
         @kitten = Kitten.new(kitten_params)
 
         if @kitten.save
-            redirect_to root, notice: "kitten was successfully created"
+            redirect_to root_path, notice: "kitten was successfully created"
         else
             render :new, status: :unprocessable_entity
         end
     end
 
     def edit
+
     end
 
     def update
         @kitten = Kitten.find(params[:id])
 
         if @kitten.update(kitten_params)
-            redirect_to root, notice: "Kitten was successfully updated"
+            redirect_to root_path, notice: "Kitten was successfully updated"
         else
             render :edit, status: :unprocessable_entity
         end
     end
 
     def destroy
-        @kitten.destroy
-        notice: "Kitten was successfully deleted"
+        @kitten = Kitten.find(params[:id])
+        
+        if @kitten.present?
+            @kitten.destroy
+        end
+        redirect_to root_path, notice: "Kitten was successfully deleted"
+        
     end
 
     def kitten_params
